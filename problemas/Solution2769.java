756b3683712836c2f6a9117f933a22be0aa83aa7
Sergio Jiménez <sergiojimenezfernandez315@gmail.com>
2025-03-30 15:30:42 +0200

    new sol

---
+++ problemas/Solution2769.java
@@ -0,0 +1,14 @@
+package problemas;
+
+public class Solution2769 {
+    public static void main(String[] args) {
+        theMaximumAchievableX(4, 1);
+    }
+
+    public static int theMaximumAchievableX(int num, int t) {
+        
+
+        return num + (2 * t);
+        
+    }
+}
