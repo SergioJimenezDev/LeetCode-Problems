0d9122be0409a4d3ba601326f15e308a5e3f51d8
Sergio Jiménez <sergiojimenezfernandez315@gmail.com>
2025-03-30 19:49:42 +0200

    sol

--- problemas/Solution3289.java
+++ problemas/Solution3289.java
@@ -1,37 +1,39 @@
 package problemas;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Solution3289 {
     public static void main(String[] args) {
-        
-        System.out.println(getSneakyNumbers(new int[]{0,1,1,0}));
 
-        for (int string : getSneakyNumbers(new int[]{0,1,1,0})) {
+        System.out.println(getSneakyNumbers(new int[] { 0, 1, 1, 0 }));
+
+        for (int string : getSneakyNumbers(new int[] { 0, 1, 1, 0 })) {
             System.out.println(string);
         }
     }
 
     public static int[] getSneakyNumbers(int[] nums) {
-        
-        int[] ans = new int[2];
+
+        Set<Integer> ans = new HashSet<>();
+
         int counter = 0;
-        int counterarray = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 0; j < nums.length / 2; j++) {
-                if (nums[i] == nums[j]){
+            for (int j = 0; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
                     counter++;
-                    if (counter == 2){
-                        ans[counterarray] = nums[i];
-                        counterarray++;
+                    if (counter == 2) {
+                        ans.add(nums[i]);
                         counter = 0;
 
                     }
+
                 }
             }
             counter = 0;
         }
 
-        return ans;
-
+        return ans.stream().mapToInt(Integer::intValue).toArray();
     }
 }
