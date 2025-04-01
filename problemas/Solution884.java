f3438bc79484eb88897451ef3b16bad893dc7494
Sergio Jiménez <sergiojimenezfernandez315@gmail.com>
2025-04-01 14:32:55 +0200

    sol

--- problemas/Solution884.java
+++ problemas/Solution884.java
@@ -1,65 +1,47 @@
 package problemas;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Solution884 {
     public static void main(String[] args) {
-        System.out.println(uncommonFromSentences("apple apple", "banana"));
+        System.out.println(uncommonFromSentences("this apple is sweet", "this apple is sour"));
+
+        for (String s : uncommonFromSentences("this apple is sweet", "this apple is sour")) {
+            System.out.println(s);
+        }
     }
 
     public static String[] uncommonFromSentences(String s1, String s2) {
 
         String[] array1 = s1.split(" ");
         String[] array2 = s2.split(" ");
+
+        Map<String, Integer> contador = new HashMap<>();
+
+        for (String palabra : array1) {
+            contador.put(palabra, contador.getOrDefault(palabra, 0) + 1);
+        }
+
+        for (String palabra : array2) {
+            contador.put(palabra, contador.getOrDefault(palabra, 0) + 1);
+        }
+
         List<String> lista = new ArrayList<>();
-        Boolean encontrado = false;
-
-        for (int i = 0; i < array1.length; i++) {
-            for (int j = 0; j < array2.length; j++) {
-                if (array1[i].equals(array2[j])) {
-                    encontrado = true;
-                } else if (!array1[i].equals(array2[j]) && !s1.contains(array2[j])) {
-                    if (!lista.contains(array2[j]))
-                        lista.add(array2[j]);
-                }
-
+        for (Map.Entry<String, Integer> entry : contador.entrySet()) {
+            if (entry.getValue() == 1) { // Solo añadimos las palabras que aparecen una vez en total
+                lista.add(entry.getKey());
             }
-            if (!encontrado) {
-                lista.add(array1[i]);
-
-            }
-            encontrado = false;
-
         }
 
-        int counter = 0;
-
-        for (int i = 0; i < lista.size(); i++) {
-            for (int j = 0; j < lista.size(); j++) {
-                if (lista.get(i).equals(lista.get(j))){
-                    counter++;
-                }
-                if (counter > 1){
-                    lista.remove(lista.get(i));
-                }
-            }
-            counter = 0;
-        }
-
-        String[] result = new String[lista.size()];
-
-        for (String string : lista) {
-            System.out.println(string);
-        }
-
-        for (int i = 0; i < lista.size(); i++) {
-            result[i] = lista.get(i);
+        for (Integer x : contador.values()) {
+            System.out.println(x);
         }
 
 
-
-        return result;
+        return lista.toArray(new String[0]);
 
     }
 }
